31b8247b0030e036ecb85f17ecedee6f8a26717a
zz <2418184580@qq.com>
2022-10-31 18:27:02 +0800

    Use `next/script`

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 import { CallToAction } from '@/components/CallToAction'
 import { Faqs } from '@/components/Faqs'
@@ -12,13 +13,13 @@ import { Testimonials } from '@/components/Testimonials'
 export default function Home() {
   return (
     <>
+      <Script src="https://cdn.usefathom.com/script.js" data-site="GCGAUKHE" />
       <Head>
         <title>Elon Code Review - Print it out and Elon Musk will check it</title>
         <meta
           name="description"
           content="Print out your last 30/60 days of code and Elon Musk will review it."
         />
-        <script src="https://cdn.usefathom.com/script.js" data-site="GCGAUKHE" defer></script>
         <link rel="canonical" href="https://eloncodereview.com" />
       </Head>
       <Header />
