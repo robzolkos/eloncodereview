d637aeab86fbb0abf1cf3fdd32d0f52d0c0659d5
Gravy59 <gravy@mailsire.com>
2022-11-14 21:59:13 -0700

    remove src entirely

--- src-old/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-export function Logo(props) {
-  return (
-    <div className="flex items-center">
-      <span className="text-3xl">🖨</span>
-      <span className="ml-2 text-2xl font-bold">Elon Code Review</span>
-    </div>
-  )
-}
