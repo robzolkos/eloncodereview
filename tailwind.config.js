59823a6c44e253b682db960c0ac492d374cb3cc9
Gravy59 <gravy@mailsire.com>
2022-11-15 17:31:41 -0700

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html'],
+  content: ['./**/*.html'],
   theme: {
     fontSize: {
       xs: ['0.75rem', { lineHeight: '1rem' }],
