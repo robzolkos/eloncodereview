3a4e7101ec7865c5267477b8303774edde04f16c
Gravy59 <gravy@mailsire.com>
2022-11-20 13:25:33 -0700

    feat!: make some MASSIVE changes and upgrade to vite

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 3000
+EXPOSE 5173
 
 CMD [ "npm", "run", "dev" ]
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        register: resolve(__dirname, "register.html"),
+      },
+    },
+  },
+});
